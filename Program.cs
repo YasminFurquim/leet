6390de86fef86a4de8576ad0f631e48e109cba76
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-17 20:36:49 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+string yasmin;
+
+Console.WriteLine("tradutor simples para a linguagem Leet Speak.");
+yasmin = Console.ReadLine()!.Replace("A","4").ToUpper()
+.Replace("E","3")
+.Replace("I","1")
+.Replace("L","1")
+.Replace("O","0")
+.Replace("T","7")
+.Replace("S","5");
+
+Console.WriteLine($"{yasmin}");
